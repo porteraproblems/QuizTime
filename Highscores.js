7d8b28ba6aa95090c2d5d2d5d7cdebb00a146b66
Alexander Portera <aportera3@gmail.com>
2020-10-30 15:59:45 -0400

    added variables and eventlistener to highscore js

---
+++ Highscores.js
@@ -0,0 +1,7 @@
+var highScore = document.querySelector("#highScore");
+var goBack = document.querySelector("#goBack");
+var clear = document.querySelector("#clear");
+
+goBack.addEventListener("click", function() {
+    window.location.replace("index.html");
+})
